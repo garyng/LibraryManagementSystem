fdb7882fd00fd53bb6a29e5bcae300e44f9c3bf9
GaryNg <garyng.zhongbo@gmail.com>
2017-09-07 08:55:02 +0800

    feat(Login): add login view and view model

--- src/Libraryman.Wpf/Login/LoginViewModel.cs
+++ src/Libraryman.Wpf/Login/LoginViewModel.cs
@@ -5,13 +5,17 @@ using System.Threading.Tasks;
 using Bogus;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Libraryman.Common.Result;
+using Libraryman.Entity;
 using Libraryman.Wpf.Extensions;
 using Libraryman.Wpf.Navigation;
+using Libraryman.Wpf.Service;
 
 namespace Libraryman.Wpf.Login
 {
 	public class LoginViewModel : ViewModelBase
 	{
+		private readonly IAuthenticationService _authentication;
 		private string _staffId;
 
 		public string StaffId
@@ -44,24 +48,37 @@ namespace Libraryman.Wpf.Login
 			set => Set(ref _isLoginSuccessful, value);
 		}
 
+		private string _loginErrorMessage;
+
+		public string LoginErrorMessage
+		{
+			get => _loginErrorMessage;
+			set => Set(ref _loginErrorMessage, value);
+		}
+
 
 		public RelayCommand LoginCommand { get; set; }
 
-		public LoginViewModel()
+		public LoginViewModel(IAuthenticationService authentication)
 		{
+			_authentication = authentication;
 			LoginCommand = new RelayCommand(async () => await Login().ConfigureAwait(false), CanLogin);
 			IsLoginSuccessful = true;
 #if DEBUG
 			if (IsInDesignMode)
 			{
 				StaffId = "123123";
+				LoginErrorMessage = "Error message!";
 			}
 #endif
 		}
 
 		private async Task Login()
 		{
-
+			Result<bool> result = await _authentication.AuthenticateAsync(int.Parse(_staffId), _staffPassword)
+				.OnSuccess(r => Console.WriteLine("Success!"))
+				.OnFailure(e => { LoginErrorMessage = e.Error; }).ConfigureAwait(false);
+			IsLoginSuccessful = result.IsSuccess;
 		}
 
 		private bool CanLogin()
